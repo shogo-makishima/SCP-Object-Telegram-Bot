f3493b01d3e6e1a5563d67ec21e1463c730cec4e
shogo-makishima <48924544+shogo-makishima@users.noreply.github.com>
2020-04-06 20:37:03 +0300

    0.0.2b2f3

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def start_message(message):
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     scpText = run(SCPFoundationAPI.GetObjectByNumber(SCPFoundationAPI, "3333"))
+    print(scpText)
     bot.send_message(message.chat.id, scpText)
 
 if ("HEROKU" in list(os.environ.keys())):
